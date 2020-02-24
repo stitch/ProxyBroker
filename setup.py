be7bcfe4f4d1ffa9fb58c5ed6a6564c78fd2ce32
Elger Jonker <elger.jonker@gmail.com>
2020-02-24 17:16:15 +0100

    try to remove any reference to old attrs versions

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with codecs.open('README.rst', mode='r', encoding='utf-8') as f:
 REQUIRES = [
     'aiohttp>=3.5.4',
     'aiodns>=2.0.0',
-    'attrs==19.1.0',
+    'attrs>=19.3.0',
     'maxminddb>=1.4.1',
 ]
 SETUP_REQUIRES = ['pytest-runner>=4.4']
